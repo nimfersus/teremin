f9258187ff12c367b3ad23978e3005d702fab511
nimfersus <105855729+nimfersus@users.noreply.github.com>
2022-05-19 14:39:52 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,19 @@
+let hoehe = 0
+let ton = 0
 basic.forever(function () {
-	
+    if (hoehe == 1) {
+        basic.showLeds(`
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        ton = 659
+    } else {
+    	
+    }
 })
 basic.forever(function () {
-	
+    hoehe = Math.abs(grove.measureInCentimeters(DigitalPin.C16) / 5)
 })
